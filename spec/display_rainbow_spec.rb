6c481e232aecb893560efee3405f1285b59c1ead
Matthew Courchaine <mdcourchaine@gmail.com>
2020-01-28 21:25:31 +0000

    Automatically backed up by Learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -25,5 +25,5 @@ end
 def display_rainbow(colors)
    puts "1: #{colors[0]}, 2: #{colors[1]}, 3: #{colors[2]}, 4: #{colors[3]}, 5: #{colors[4]}, 6: #{colors[5]}, 7. #{colors[6]}"
 end
-colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+colors = ["R", "O", "Y", "G", "B", "I", "V"]
 
